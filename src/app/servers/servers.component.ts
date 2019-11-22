111d872530a9fbed99d5faa4577996ce3f6083fb
John Wang <wangqianjiang@live.com>
2019-11-21 20:45:42 -0600

    use class attribute instead of element

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+//  selector: 'app-servers', //element
+//  selector: '[app-servers]', //attribte
+  selector: '.app-servers', //class
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
